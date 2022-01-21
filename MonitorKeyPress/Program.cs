523e90c7c7a0161184fa34b1c1c61534af7cd074
Marty Grogan <owner@groganenterpriseservices.com>
2022-01-21 10:18:33 -0800

    Initial Commit

---
+++ MonitorKeyPress/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace TestKeyTrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool keyFlag=false;
+
+            while (!keyFlag)
+            {
+                Console.Write(".");
+                keyFlag = CheckForKey();
+                Thread.Sleep(500);
+            }
+        }
+
+        private static bool CheckForKey()
+        {
+            if (Console.KeyAvailable)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+;
